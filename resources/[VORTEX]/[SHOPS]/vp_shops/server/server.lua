a40c9af802527f1013834723d801d708287c7068
klandestino7 <klandestino6@hotmail.com>
2020-04-19 17:13:05 -0300

    REMOVE PASTA

--- resources/[VORTEX]/[SHOPS]/vp_shops/server/server.lua
+++
@@ -1,9 +0,0 @@
-local Tunnel = module("_core","lib/Tunnel")
-local Proxy = module("_core","lib/Proxy")
-fcRP = Proxy.getInterface("API_Core")
-fcRPCli = Tunnel.getInterface("API_Core")
-
-RegisterServerEvent('frp_shops:buyedItem')
-AddEventHandler('frp_shops:buyedItem', function(item)
-    local _source = source
-end)
