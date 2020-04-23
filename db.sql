277910061f516b8666b879d242a8a96830cffe4b
klandestino7 <cleidson-almeida@hotmail.com>
2020-04-23 11:26:58 -0300

    Update SQL

--- db.sql
+++ db.sql
@@ -22,22 +22,21 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `user_id` int(11) NOT NULL,
   `characterName` varchar(50) NOT NULL,
   `level` int(11) DEFAULT 1,
+  `age` int(11) DEFAULT 1,
   `xp` int(11) DEFAULT 0,
+  `wanted` text NOT NULL DEFAULT '{}',
   `groups` text NOT NULL DEFAULT '{}',
   `charTable` text NOT NULL DEFAULT '{}',
+  `SkinMdf` text NOT NULL DEFAULT '{}',
   `clothes` text DEFAULT '{}',
-  `weapons` text NOT NULL DEFAULT '{}',
   `is_dead` int(11) DEFAULT 0,
   PRIMARY KEY (`charid`),
   KEY `FK_characters_users` (`user_id`),
   CONSTRAINT `FK_characters_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela redm.characters: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela redm.characters: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `characters` DISABLE KEYS */;
-INSERT INTO `characters` (`charid`, `user_id`, `characterName`, `level`, `xp`, `groups`, `charTable`, `clothes`, `weapons`, `is_dead`) VALUES
-	(28, 3, 'Nome Teste', 1, 0, '{"user":true,"admin":true}', '{"hunger": "50", "thirst": "50", "model": "mp_male", "head": "517496604", "age": "18", "hair": "332193580", "teeth": "101272007", "mustache": "298392309", "position": "{\\"y\\":-1294.33,\\"z\\":52.24,\\"x\\":2642.38}", "posse": "56", "banco": "23000"}', '{"gloves": "0x10BD09BD", "hat": "0x10106532", "vests": "0x1077DF15", "boots": "0x11044269", "pants": "0x1A0CD667", "shirts": "0x10A9CF9B", "gunbelts": "0x3DF1A52", "coats": "0x105F0147", "legs": "0x8AD16414"}', '{"weapon_repeater_carbine":1}', 0),
-	(30, 3, 'Testante Haah', 1, 0, '{"user":true,"admin":true,"vip":true}', '{"hunger": "0", "thirst": "0", "legs": "453543685", "age": "19", "torso": "453543685", "model": "mp_female", "head": "301009282", "position": "{\\"x\\":1343.94,\\"z\\":49.66,\\"y\\":-1904.61}"}', '{"gunbelts": "0x4D2686CA", "neckwear": "0x122A8031", "pants": "0x114B1F63", "boots": "0x12CB6473", "shirts": "0x1178F4F4", "hat": "0x1746D236"}', '{}', 0);
 /*!40000 ALTER TABLE `characters` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela redm.chests
@@ -50,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `chests` (
   PRIMARY KEY (`id`),
   KEY `FK_chests_characters` (`charid`),
   CONSTRAINT `FK_chests_characters` FOREIGN KEY (`charid`) REFERENCES `characters` (`charid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 
 -- Copiando dados para a tabela redm.chests: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `chests` DISABLE KEYS */;
@@ -69,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `fort` (
 -- Copiando dados para a tabela redm.fort: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `fort` DISABLE KEYS */;
 INSERT INTO `fort` (`id`, `nome`, `bando`) VALUES
-	(1, 'Mercer', 56),
+	(1, 'Mercer', 0),
 	(2, 'Wallace', 0);
 /*!40000 ALTER TABLE `fort` ENABLE KEYS */;
 
@@ -88,7 +87,9 @@ BEGIN
 	ELSEIF (keyValue = 'all' && typeData = 'groups') THEN
 		SELECT groups as Value FROM characters WHERE charid = id;
 	ELSEIF (keyValue = 'all' && typeData = 'charTable') THEN
-		SELECT charTable as Value FROM characters WHERE charid = id;
+		SELECT charTable as Value FROM characters WHERE charid = id;		
+	ELSEIF (keyValue = 'all' && typeData = 'SkinMdf') THEN
+		SELECT SkinMdf as Value FROM characters WHERE charid = id;			
 	ELSEIF (keyValue = 'all' && typeData = 'modificacao') THEN
 		SELECT modificacao as Value FROM horses WHERE id = id;
 	END IF;
@@ -102,6 +103,8 @@ BEGIN
 		SELECT json_extract(clothes, CONCAT("$.", keyValue)) as Value FROM characters WHERE charid = id;
 	ELSEIF (typeData = 'charTable') THEN
 		SELECT json_extract(charTable, CONCAT("$.", keyValue)) as Value FROM characters WHERE charid = id;
+	ELSEIF (typeData = 'SkinMdf') THEN
+		SELECT json_extract(SkinMdf, CONCAT("$.", keyValue)) as Value FROM characters WHERE charid = id;
 	ELSEIF (typeData = 'modificacao') THEN
 		SELECT json_extract(modificacao, CONCAT("$.", keyValue)) as Value FROM horses WHERE id = id;
 	END IF;
@@ -114,42 +117,15 @@ CREATE TABLE IF NOT EXISTS `horses` (
   `charid` int(11) NOT NULL,
   `model` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `modif` text DEFAULT '{}',
+  `components` text NOT NULL DEFAULT '{}',
   PRIMARY KEY (`id`),
   KEY `FK_horses_characters` (`charid`),
   CONSTRAINT `FK_horses_characters` FOREIGN KEY (`charid`) REFERENCES `characters` (`charid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela redm.horses: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela redm.horses: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `horses` DISABLE KEYS */;
-INSERT INTO `horses` (`id`, `charid`, `model`, `name`, `modif`) VALUES
-	(3, 28, 'A_C_Donkey_01', 'Burrinho', ''),
-	(4, 28, 'A_C_HORSE_TURKOMAN_GOLD', 'Cavalo1', '{}'),
-	(6, 30, 'A_C_Donkey_01', 'Burrinho', ''),
-	(7, 30, 'A_C_HORSE_ARABIAN_ROSEGREYBAY', 'Cavalokkk', '');
 /*!40000 ALTER TABLE `horses` ENABLE KEYS */;
-
--- Copiando estrutura para tabela redm.inventories
-CREATE TABLE IF NOT EXISTS `inventories` (
-  `id` varchar(100) NOT NULL,
-  `charid` int(11) DEFAULT NULL,
-  `capacity` int(11) DEFAULT NULL,
-  `items` text NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `FK_inventoriers_characters` (`charid`),
-  CONSTRAINT `FK_inventories_characters` FOREIGN KEY (`charid`) REFERENCES `characters` (`charid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Copiando dados para a tabela redm.inventories: ~6 rows (aproximadamente)
-/*!40000 ALTER TABLE `inventories` DISABLE KEYS */;
-INSERT INTO `inventories` (`id`, `charid`, `capacity`, `items`) VALUES
-	('char:28', 28, 20, '{"generic_gold": 97, "consumable_apple": 18, "kit_camp_simple": 2, "generic_picareta": 1, "generic_provision_disco_ammolite": 3, "generic_ourobruto": 21, "generic_pedralavada": 12, "weapon_bow": 47, "weapon_melee_lantern_electric": 5, "weapon_pistol_volcanic": 47, "weapon_lasso": 1, "weapon_repeater_carbine": 68, "weapon_fishingrod": 0, "weapon_melee_torch": 0, "ammo_arrow": 20, "ammo_repeater": 20, "generic_money": 50260}'),
-	('char:30', 30, 20, '{"generic_money": 489590, "weapon_revolver_cattleman": 1}'),
-	('horse:3', 28, 20, '{}'),
-	('horse:4', 28, 20, '{}'),
-	('horse:6', 30, 20, '{}'),
-	('horse:7', 30, 20, '{}');
-/*!40000 ALTER TABLE `inventories` ENABLE KEYS */;
 
 -- Copiando estrutura para procedure redm.inventories
 DELIMITER //
@@ -178,6 +154,21 @@ BEGIN
 END//
 DELIMITER ;
 
+-- Copiando estrutura para tabela redm.inventories
+CREATE TABLE IF NOT EXISTS `inventories` (
+  `id` varchar(100) NOT NULL,
+  `charid` int(11) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `items` text NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_inventoriers_characters` (`charid`),
+  CONSTRAINT `FK_inventories_characters` FOREIGN KEY (`charid`) REFERENCES `characters` (`charid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela redm.inventories: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `inventories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventories` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela redm.jail
 CREATE TABLE IF NOT EXISTS `jail` (
   `id` int(11) NOT NULL DEFAULT 0,
@@ -205,9 +196,32 @@ CREATE TABLE IF NOT EXISTS `posses` (
 
 -- Copiando dados para a tabela redm.posses: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `posses` DISABLE KEYS */;
-INSERT INTO `posses` (`id`, `charid`, `members`, `name`) VALUES
-	(56, 28, '{"28":1}', 'BICHO');
 /*!40000 ALTER TABLE `posses` ENABLE KEYS */;
+
+-- Copiando estrutura para procedure redm.procInventory
+DELIMITER //
+CREATE PROCEDURE `procInventory`(
+	IN `iid` VARCHAR(20),
+	IN `charid` INT(8),
+	IN `slot` INT(8),
+	IN `itemId` VARCHAR(100),
+	IN `itemAmount` INT(8),
+	IN `procType` VARCHAR(8)
+)
+BEGIN
+    IF (procType = "update") THEN
+        UPDATE inventories SET items = JSON_SET(items, 'CONCAT("$.", slot)[0]', itemId, 'CONCAT("$.", slot)[1]', itemAmount) WHERE id = iid;
+    ELSEIF (procType = "remove") THEN
+        UPDATE inventories SET items = JSON_REMOVE(items, CONCAT("$.", slot)) WHERE id = iid;
+    ELSEIF (procType = "select") THEN
+        SELECT * from inventories WHERE id = iid;
+    ELSEIF (procType = "insert") THEN
+        INSERT INTO inventories(id, charid, capacity, items) VALUES (iid, charid, 20, "{}");
+    ELSEIF (procType = "clear") THEN
+        UPDATE inventories SET items = '{}' WHERE id = iid and charid = charid;
+    END IF;
+END//
+DELIMITER ;
 
 -- Copiando estrutura para tabela redm.quests
 CREATE TABLE IF NOT EXISTS `quests` (
@@ -239,6 +253,8 @@ BEGIN
 		UPDATE characters SET wanted = JSON_REMOVE(wanted, CONCAT("$.", keyValue)) WHERE charid = id;
 	ELSEIF (typeData = 'charTable') THEN
 		UPDATE characters SET charTable = JSON_REMOVE(charTable, CONCAT("$.", keyValue)) WHERE charid = id;
+	ELSEIF (typeData = 'SkinMdf') THEN
+		UPDATE characters SET SkinMdf = JSON_REMOVE(SkinMdf, CONCAT("$.", keyValue)) WHERE charid = id;
 	ELSEIF (typeData = 'modificacao') THEN
 		UPDATE horses SET modificacao = JSON_REMOVE(modificacao, CONCAT("$.", keyValue)) WHERE charid = id;
 	END IF;
@@ -262,6 +278,8 @@ BEGIN
 		UPDATE characters SET wanted = JSON_SET(wanted, CONCAT("$.", keyValue), valueKey) WHERE charid = id;			
 	ELSEIF (typeData = 'charTable') THEN
 		UPDATE characters SET charTable = JSON_SET(charTable, CONCAT("$.", keyValue), valueKey) WHERE charid = id;		
+	ELSEIF (typeData = 'SkinMdf') THEN
+		UPDATE characters SET SkinMdf = JSON_SET(SkinMdf, CONCAT("$.", keyValue), valueKey) WHERE charid = id;
 	ELSEIF (typeData = 'modificacao') THEN
 		UPDATE horses SET modificacao = JSON_SET(modif, CONCAT("$.", keyValue), valueKey) WHERE id = id;
 	END IF;
@@ -277,12 +295,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   KEY `identifier` (`identifier`),
   CONSTRAINT `FK_users_whitelist` FOREIGN KEY (`identifier`) REFERENCES `whitelist` (`identifier`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- Copiando dados para a tabela redm.users: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`user_id`, `identifier`, `name`, `banned`) VALUES
-	(3, 'steam:11000010596ee06', 'KlandesTino', 0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela redm.whitelist
