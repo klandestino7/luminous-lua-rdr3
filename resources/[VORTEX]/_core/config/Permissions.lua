d034367b84b92da9908e80f95c0123ac33aee6cb
klandestino7 <klandestino6@hotmail.com>
2020-05-28 01:11:33 -0300

    Update Permission

--- resources/[VORTEX]/_core/config/Permissions.lua
+++ resources/[VORTEX]/_core/config/Permissions.lua
@@ -2,8 +2,10 @@ config_file_GROUPS = {
  -- ["none"]      = 0,      -- 0
     ["admin"]     = 1 << 0, -- 1
     ["moderator"] = 1 << 1, -- 2
-    ["sheriff"]   = 1 << 2, -- 8
-    ["trooper"]   = 1 << 3, -- 16vc
+    ["sheriff"]   = 1 << 2, -- 4
+    ["trooper"]   = 1 << 3, -- 8vc
+    ["medic"]   = 1 << 4, -- 16vc
+ --   ["medic"]   = 1 << 5, -- 32vc
     -- ["house:1"]
     -- ["house:2"]
     -- ["house:3"]
