643eca0611f86aa2135e5cb26e5c4040e3e0b4d1
slashkeyvalue <59461439+slashkeyvalue@users.noreply.github.com>
2020-05-28 05:58:46 -0300

    fix: typo

--- resources/[VORTEX]/_core/config/Permissions.lua
+++ resources/[VORTEX]/_core/config/Permissions.lua
@@ -1,11 +1,10 @@
 config_file_GROUPS = {
- -- ["none"]      = 0,      -- 0
-    ["admin"]     = 1 << 0, -- 1
+    -- ["none"] = 0,        -- 0
+    ["admin"] =     1 << 0, -- 1
     ["moderator"] = 1 << 1, -- 2
-    ["sheriff"]   = 1 << 2, -- 4
-    ["trooper"]   = 1 << 3, -- 8vc
-    ["medic"]   = 1 << 4, -- 16vc
- --   ["medic"]   = 1 << 5, -- 32vc
+    ["sheriff"] =   1 << 2, -- 4
+    ["trooper"] =   1 << 3, -- 8
+    ["medic"] =     1 << 4  -- 16
     -- ["house:1"]
     -- ["house:2"]
     -- ["house:3"]
@@ -13,5 +12,5 @@ config_file_GROUPS = {
 
 config_file_INHERITANCE = {
     ["admin"] = "moderator",
-    ["sheriff"] = "tropper",
+    ["sheriff"] = "trooper"
 }
