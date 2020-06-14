4c9fab3a19f5b71c68a5dd480dabe793001c12c8
klandestino7 <klandestino6@hotmail.com>
2020-06-14 18:54:05 -0300

    modificaçoes do server....

--- resources/[VORTEX]/[SYSTEM]/vp_heal/config.lua
+++ resources/[VORTEX]/[SYSTEM]/vp_heal/config.lua
@@ -5,8 +5,10 @@ Config.Shoptext = 'Aperte (E) para se curar'
 Config.Coords = {
 	Codds = {
 		vector3(-285.87, 805.07, 119.39),
+		vector3(2730.295,-1231.783,50.376)
 	},
 	Locations = {
 		vector3(-285.87, 805.07, 119.39),
+		vector3(2730.295,-1231.783,50.376)
 	}
 }
