59af53c98ed68ca04e00412e6d79bac6d20f17a2
slashkeyvalue <59461439+slashkeyvalue@users.noreply.github.com>
2020-06-15 07:58:08 -0300

    feat: platinhas

--- resources/[VORTEX]/[SYSTEM]/research/client_js.js
+++ resources/[VORTEX]/[SYSTEM]/research/client_js.js
@@ -19,7 +19,7 @@ exports('DataViewNativeGetScenarioPointsCloseToCoords', (x, y, z, radius, size) 
     // dataView.setInt32(0, 3);
     // dataView.setInt32(3, );
 
-    Citizen.invokeNative("0x345EC3B7EBDE1CB5", x, y, z, radius, view, size, Citizen.returnResultAnyway());
+    letCitizen.invokeNative("0x345EC3B7EBDE1CB5", x, y, z, radius, view, size, Citizen.returnResultAnyway());
     let out = new Int32Array(buffer);
 
     // console.log(out);
@@ -27,51 +27,19 @@ exports('DataViewNativeGetScenarioPointsCloseToCoords', (x, y, z, radius, size) 
     return out;
 });
 
-exports('DataViewNetowrk', () => {
+exports('DataViewNetowrk', (comp) => {
 
 
-    // Global_1572887->f_342.f_4 = 4;
-    // Global_1572887->f_342.f_21 = iParam0;
-    // Global_1572887->f_342.f_5 = 18;
-    // Global_1572887->f_342.f_25 = 0;
-    // Global_1572887->f_342.f_26 = 0;
-
-    let buffer = new ArrayBuffer(21 * 8);
+    let buffer = new ArrayBuffer(256);
     let view = new DataView(buffer);
 
-    view.setInt32(0, 30.0, true); // f_1
-    view.setInt32(8, 0.0, true); // f_2
-    view.setInt32(16, 0.5, true); // f_3
-    view.setInt32(24, 0.0, true); // f_4
-    view.setInt32(32, 0, true); // f_5
-    view.setInt32(40, 0, true); // f_6
-    view.setInt32(48, 0, true); // f_7
-    view.setInt32(56, 0.0, true); // f_8
-    view.setInt32(64, 0.0, true); // f_9
-    view.setInt32(72, 0, true); // f_10
-    view.setInt32(80, 0, true); // f_11
-    view.setInt32(88, 0, true); // f_12
-    view.setInt32(96, 1.0, true); // f_13
-    view.setInt32(104, 0, true); // f_14
-    view.setInt32(112, 0, true); // f_15
-    view.setInt32(120, 3, true); // f_16
-    view.setInt32(128, 0.0, true); // f_17
-    view.setInt32(136, 0, true); // f_18
-    view.setInt32(144, 0, true); // f_19
-    view.setInt32(152, 0.5, true); // f_20
-    view.setInt32(160, 1.0, true); // f_21
-    // view.setInt32(168, 0.0, true); // f_22
-    // view.setInt32(176, 0.0, true); // f_23
-    // view.setInt32(184, 0.0, true); // f_24
-    // view.setInt32(192, 0.0, true); // f_25
-    // view.setInt32(200, 0.0, true); // f_26
-    // view.setInt32(208, 0.0, true); // f_27
-    // view.setInt32(216, 0.0, true); // f_28
-    // view.setInt32(224, 0.0, true); // f_29
-    // view.setInt32(232, 0.0, true); // f_30
-    // view.setInt32(240, 0.0, true); // f_31
+    let w = Citizen.invokeNative("0x5B4BBE80AD5972DC", comp, GetEntityCoords(PlayerPedId()), 0.0, 0, view, -1);
 
-    Citizen.invokeNative("0xF3735ACD11ACD501", PlayerPedId(), view);
+    // Citizen.invokeNative("0x5758B1EE0C3FD4AC", w, 0); // DELETE_PLANT
+    let z = Citizen.invokeNative("0x96C6ED22FB742C3E", w, GetEntityCoords(PlayerPedId()));
 
     let out = new Int32Array(buffer);
+    console.log(z);
+    console.log(w);
+    console.log(out);
 });
