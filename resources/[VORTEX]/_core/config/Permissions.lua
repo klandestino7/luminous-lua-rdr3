2e23296d2048171b9405a3b63d0d73303a0e751b
slashkeyvalue <59461439+slashkeyvalue@users.noreply.github.com>
2020-06-17 00:46:48 -0300

    tweak: house groups

--- resources/[VORTEX]/_core/config/Permissions.lua
+++ resources/[VORTEX]/_core/config/Permissions.lua
@@ -4,10 +4,14 @@ config_file_GROUPS = {
     ["moderator"] = 1 << 1, -- 2
     ["sheriff"] =   1 << 2, -- 4
     ["trooper"] =   1 << 3, -- 8
-    ["medic"] =     1 << 4  -- 16
-    -- ["house:1"]
-    -- ["house:2"]
-    -- ["house:3"]
+    ["medic"] =     1 << 4,  -- 16
+    ["house:1"] =   1 << 5,
+    ["house:2"] =   1 << 6,
+    ["house:3"] =   1 << 7,
+    ["house:4"] =   1 << 8,
+    ["house:5"] =   1 << 9,
+    ["house:6"] =   1 << 10,
+    ["house:7"] =   1 << 11,
 }
 
 config_file_INHERITANCE = {
