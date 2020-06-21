e0cf534427f06b90575d4ff1edb5dbb1094fae95
klandestino7 <klandestino6@hotmail.com>
2020-06-21 17:27:48 -0300

    Mais itens para crafting

--- resources/[VORTEX]/[SYSTEM]/vp_crafting/_cfg.lua
+++ resources/[VORTEX]/[SYSTEM]/vp_crafting/_cfg.lua
@@ -2,6 +2,32 @@ Config = {
     ["gold"] = {
         ["gold_nugget"] = 36,
     },
+    ["campfiresmall"] ={
+        ["row_rock"] = 3,
+        ["log"] = 3
+    },
+    ["campfiremed"] ={
+        ["row_rock"] = 5,
+        ["log"] = 5
+    },
+    ["bow"] = {
+        ["stick"] = 2,
+        ["ropecuted"] = 1
+    },
+    ["ammo_arrow"] = {
+        ["stick"] = 2,
+        ["bones"] = 1
+    },
+    ["ropecuted"] = {
+        ["roperoll"] = 1,
+    },
+    
+    ["ropecuted"] = {
+        ["roperoll"] = 1,
+    },
+
+    
+
     -- ["shotgun_pump"] = {
     --     ["raw_gold"] = 10,
     --     ["gold"] = 10
