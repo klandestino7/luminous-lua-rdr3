f8b950b11859824c0e6a79f21eee5174e921d011
klandestino7 <klandestino6@hotmail.com>
2020-06-21 17:26:25 -0300

    Fogueira

--- resources/[TEST]/campfire/client.lua
+++ resources/[TEST]/campfire/client.lua
@@ -10,11 +10,30 @@ RegisterCommand('campfire', function(source, args, rawCommand)
     end
     local playerPed = PlayerPedId()
     TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 30000, true, false, false, false)
-    exports['progressBars']:startUI(30000, "Creando hogera")
-    Citizen.Wait(30000)
+   -- exports['progressBars']:startUI(30000, "Creando hogera")
+    Citizen.Wait(1000)
     ClearPedTasksImmediately(PlayerPedId())
     local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, -1.55))
-    local prop = CreateObject(GetHashKey("p_campfire02x"), x, y, z, true, false, true)
+    local prop = CreateObject(GetHashKey("p_campfire03x"), x, y, z, true, false, true)
+    SetEntityHeading(prop, GetEntityHeading(PlayerPedId()))
+    PlaceObjectOnGroundProperly(prop)
+    campfire = prop
+end, false)
+
+
+RegisterCommand('campfire2', function(source, args, rawCommand)
+    if campfire ~= 0 then
+        SetEntityAsMissionEntity(campfire)
+        DeleteObject(campfire)
+        campfire = 0
+    end
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 30000, true, false, false, false)
+   -- exports['progressBars']:startUI(30000, "Creando hogera")
+    Citizen.Wait(1000)
+    ClearPedTasksImmediately(PlayerPedId())
+    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, -1.55))
+    local prop = CreateObject(GetHashKey("p_campfire04x"), x, y, z, true, false, true)
     SetEntityHeading(prop, GetEntityHeading(PlayerPedId()))
     PlaceObjectOnGroundProperly(prop)
     campfire = prop
