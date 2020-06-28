9162226957e4fe31e86ee33d7f9971e198adbadd
klandestino7 <klandestino6@hotmail.com>
2020-06-28 14:28:50 -0300

    Barra de progresso para algumas ações

---
+++ resources/vp_progbar/fxmanifest.lua
@@ -0,0 +1,30 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+fx_version 'adamant'
+game 'common'
+
+ui_page('html/index.html') 
+
+client_scripts {
+    'client/functions.lua',
+    'client/events.lua',
+}
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/css/bootstrap.min.css',
+    'html/fonts/crock.ttf',
+    'html/fonts/robotoslab.ttf',
+    'html/img/bar.png',
+    'html/img/barv.png',
+    'html/js/script.js',
+    'html/js/jquery.min.js',
+}
+
+exports {
+    'Progress',
+    'ProgressWithStartEvent',
+    'ProgressWithTickEvent',
+    'ProgressWithStartAndTick'
+}
