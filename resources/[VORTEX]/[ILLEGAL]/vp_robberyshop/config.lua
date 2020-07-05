90613df415996b38c69c9b69a9f03f275f3f8527
klandestino7 <klandestino6@hotmail.com>
2020-07-05 00:19:58 -0300

    uPDATE

--- resources/[VORTEX]/[ILLEGAL]/vp_robberyshop/config.lua
+++ resources/[VORTEX]/[ILLEGAL]/vp_robberyshop/config.lua
@@ -19,10 +19,10 @@ Config.moneyReward = {
 }
 
 Config.ATMS = {
-	{ id = 1,		x =		-324.17,		y =		804.11,			z = 117.882,	h = 277.0,		seconds = 60,	wasRobbed = 60 },
-	{ id = 2,		x =		-288.108,		y =		804.965,		z = 119.386,	h = 277.0,		seconds = 60,	wasRobbed = 60 },
-	{ id = 3,		x =		3025.458,		y =		561.300,		z = 44.724,		h = 262.0,		seconds = 60,	wasRobbed = 60 },
-	{ id = 4,		x =		1330.086,		y =		-1293.583,		z = 77.021,		h = 59.0,		seconds = 60,	wasRobbed = 60 },
-	{ id = 5,		x =		2639.819,		y =		-1227.647,		z = 53.380,		h = 84.22,		seconds = 60,	wasRobbed = 60 },
-	{ id = 6,		x =		2512.297,		y =		-1455.925,		z = 46.312,		h = 85.99,		seconds = 60,	wasRobbed = 60 },
+--	{ id = 1,		x =		-324.17,		y =		804.11,			z = 117.882,	h = 277.0,		seconds = 60,	wasRobbed = 60 },
+--	{ id = 2,		x =		-288.108,		y =		804.965,		z = 119.386,	h = 277.0,		seconds = 60,	wasRobbed = 60 },
+	{ id = 1,		x =		3025.458,		y =		561.300,		z = 44.724,		h = 262.0,		seconds = 60,	wasRobbed = 60 },
+	{ id = 2,		x =		1330.086,		y =		-1293.583,		z = 77.021,		h = 59.0,		seconds = 60,	wasRobbed = 60 },
+	{ id = 3,		x =		2639.819,		y =		-1227.647,		z = 53.380,		h = 84.22,		seconds = 60,	wasRobbed = 60 },
+	{ id = 4,		x =		2512.297,		y =		-1455.925,		z = 46.312,		h = 85.99,		seconds = 60,	wasRobbed = 60 },
 }
