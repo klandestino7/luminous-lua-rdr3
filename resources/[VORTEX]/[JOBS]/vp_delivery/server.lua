1899dfd95c70e5dda9dcde813070bb151d2574ee
klandestino7 <klandestino6@hotmail.com>
2020-07-23 14:16:30 -0300

    Correção Correios

--- resources/[VORTEX]/[JOBS]/vp_delivery/server.lua
+++ resources/[VORTEX]/[JOBS]/vp_delivery/server.lua
@@ -12,20 +12,20 @@ AddEventHandler(
         local User = API.getUserFromSource(_source)
         local Inventory = User:getCharacter():getInventory()
 
-        Inventory:addItem('money', payment*100)
+        Inventory:addItem('money', payment)
     end
 )
 
-RegisterServerEvent("VP:DELIVERY:paiefinale") --Paie "bonus" lors de la fin de service
-AddEventHandler("VP:DELIVERY:paiefinale", function()
-    local _source = source
-    local User = API.getUserFromSource(_source)
-    local Inventory = User:getCharacter():getInventory()
+-- RegisterServerEvent("VP:DELIVERY:paiefinale") --Paie "bonus" lors de la fin de service
+-- AddEventHandler("VP:DELIVERY:paiefinale", function()
+--     local _source = source
+--     local User = API.getUserFromSource(_source)
+--     local Inventory = User:getCharacter():getInventory()
 
-    local payment = math.random(1, 5)
+--     local payment = math.random(1, 5)
     
-    Inventory:addItem('money', payment*100)
+--     Inventory:addItem('money', payment*100)
 
-    TriggerClientEvent('VP:NOTIFY:Simple', source, 'Aqui está uma bonificação $'.. payment, 10000)  
+--     TriggerClientEvent('VP:NOTIFY:Simple', source, 'Aqui está uma bonificação $'.. payment, 10000)  
 
-end)
+-- end)
